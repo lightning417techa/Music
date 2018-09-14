b858dbb4a97090a1369aa17a4ca13ae81ddf6c4a
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-14 18:16:33 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -60,7 +60,7 @@ function Shuffle() {
   audiotitle.innerHTML = x.title;
   audioartist.innerHTML = artists[s];
   
-  alert(albumart[s]);
+  alert(s);
   if (albumart[s] == "") {
        image.src = "https://iplock.weebly.com/uploads/9/5/7/3/95731436/p164.png";
      } else {
