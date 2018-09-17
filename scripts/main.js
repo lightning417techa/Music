f7b91eb303d049345ef080883288903793726c15
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-17 16:51:29 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -84,7 +84,7 @@ switch (window.orientation) {
    case 90:  
      // Landscape  (Counterclockwise)
        //alert("Landscape (Counter-Clockwise) 90°");
-     document.getElementById("container").setAttribute("style", "position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 0px; left: 0; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");
+     document.getElementById("container").setAttribute("style", "position: relative; width: 330px; height:100%; background: #333; overflow: auto; margin: 0px; left: 0; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");
      zoomOutMobile();
      break;
      }
