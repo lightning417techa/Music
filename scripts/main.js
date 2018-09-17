2d837655acc55a90a25b9ded3956e18b90d8a875
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-17 16:34:57 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -59,8 +59,7 @@ function zoomOutMobile() {
   var viewport = document.querySelector('meta[name="viewport"]');
 
   if ( viewport ) {
-    viewport.content = "initial-scale=0.1";
-    viewport.content = "width=1200";
+    viewport.content = "width=device-width, initial-scale=1.0";
   }
 }
 
