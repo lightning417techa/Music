dd609e2c7d31465c1a2d00a99d61d315fd189be6
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-17 08:34:36 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -57,22 +57,22 @@ function readDeviceOrientation() {
 switch (window.orientation) {  
   case 0:  
     // Portrait 
-    alert("Portrait 0°");
+      //alert("Portrait 0°");
     document.getElementById("container").setAttribute("style", "position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 20px auto; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");                                                                                           
     break;      
   case 180:  
     // Portrait (Upside-down)
-    alert("Portrait (Upside Down) 180°");
+      //alert("Portrait (Upside Down) 180°");
     document.getElementById("container").setAttribute("style", "position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 20px auto; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");
     break; 
   case -90:  
     // Landscape (Clockwise)
-    alert("Landscape (Clockwise) -90°");
+      //alert("Landscape (Clockwise) -90°");
     document.getElementById("container").setAttribute("style", " position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 0; left: 0; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");
     break;  
   case 90:  
     // Landscape  (Counterclockwise)
-    alert("Landscape (Counter-Clockwise) 90°");
+      //alert("Landscape (Counter-Clockwise) 90°");
     document.getElementById("container").setAttribute("style", " position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 0; left: 0; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");
     break;
     }
