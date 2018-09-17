12f400cc73435d18ae2130afed5bcd861a20a2f6
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-17 08:53:14 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,35 @@ document.getElementById("Dropbutton").classList.remove("dropbtnclick");
 function drop() {
 }
 
+
+
+window.onload = function() {
+  var ctx = new AudioContext();
+  var audio = document.getElementById('myAudio');
+  var audioSrc = ctx.createMediaElementSource(audio);
+  var analyser = ctx.createAnalyser();
+  // we have to connect the MediaElementSource with the analyser 
+  audioSrc.connect(analyser);
+  // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
+ 
+  // frequencyBinCount tells you how many values you'll receive from the analyser
+  var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+ 
+  // we're ready to receive some data!
+  // loop
+  function renderFrame() {
+     requestAnimationFrame(renderFrame);
+     // update data in frequencyData
+     analyser.getByteFrequencyData(frequencyData);
+     // render frame based on values in frequencyData
+     // console.log(frequencyData)
+  }
+  audio.start();
+  renderFrame();
+};
+
+
+
 window.onclick = function(event, clicked_id) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
