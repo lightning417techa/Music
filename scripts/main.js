39d94a3ee987a22214f4f2aacb164bffe57fb183
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-18 17:55:33 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ document.getElementById("Dropbutton").classList.toggle("dropbtnclick");
 document.getElementById("Dropbutton").classList.toggle("dropbtnpos");
 document.getElementById("myDropdown").classList.toggle("show");
   document.getElementById("PlaylistDropdown").classList.toggle("show");
-  document.getElementById("MiniPlayer").style.setAttribute("style","width: 200px; position:absolute; top:100vh; transform:translateY(-100%);");
+  document.getElementById("MiniPlayer").style.setAttribute("style","width: 200px; position:absolute; bottom: 0%;");
   
 } else {
 i--;
