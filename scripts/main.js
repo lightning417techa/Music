2253e7e375a0793eccc9b80187a5668fbf88195b
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-18 18:34:01 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -99,6 +99,24 @@ switch (window.orientation) {
 readDeviceOrientation();
 window.onorientationchange = readDeviceOrientation;
 
+
+function search() {
+  var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("a");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
 function Shuffle() {
   var s = Math.floor(Math.random() * 22) + 1;
 
