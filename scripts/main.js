349ebb5eea3e86f23dfcb7d47fd54c2ab530bce4
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-19 15:27:23 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -194,6 +194,12 @@ function createCanvas ( w, h ) {
     var newCanvas = document.createElement('canvas');
     newCanvas.width  = w; newCanvas.height = h;
     newCanvas.id = "VisualizerCanvas";
+  
+    var VisualizerDuration = document.createElement('input');
+     VisualizerDuration.id = "VisualizerSlider";
+     VisualizerDuration.width = w; VisualizerDuration.height = h;
+     VisualizerDuration.setAttribute("type", "range");
+    return VisualizerDuration;
     return newCanvas;
 };
 
