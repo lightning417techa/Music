b883bb2534f8b8e3b0d94a4a9dd48c5a638d9b3e
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-19 15:13:20 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -199,6 +199,7 @@ function createCanvas ( w, h ) {
     VisualizerDuration.id = "VisualizerSlider";
     VisualizerDuration.setAttribute("style", "width: 500px; height: 20px; color: white; position: relative; left: 0; z-index: 10;");
     VisualizerDuration.setAttribute("type", "range");
+    newCanvas.appendChild(VisualizerDuration);
     return newCanvas;
 };
 
