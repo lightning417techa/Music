4e6666498c6df37b37d7ff90b57edd09d798febd
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-21 15:31:17 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -21,7 +21,7 @@ function startUI() {
      newDiv.id = "33";
      newDiv.innerHTML = titles[newDiv.id] + " -- " + artists[newDiv.id];
      document.getElementById("SongBtns").appendChild(newDiv);
-     newDiv.onclick = Playbutton(newDiv.id);
+     newDiv.onclick = "Playbutton(newDiv.id)";
 }
 
 function Update() {
