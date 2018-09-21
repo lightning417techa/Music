e3d26d311036659e73f95e4f6d77b3242fca50d8
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-21 13:32:58 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,7 @@ function Update() {
   let Updateimage = document.createElement("img");
   Updateimage.id = "UIMG";
   Updateimage.src = "https://iplock.weebly.com/uploads/9/5/7/3/95731436/p164.png";
-  Updateimage.style.width = "400px";
+  Updateimage.style.width = "200px";
   Updateimage.classList.toggle("UpdateIMG");
   UpdateContainer.appendChild(Updateimage);
   
