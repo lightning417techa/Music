6e8296e1002c14f706fbe0e60d0c2c975eea0564
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-22 11:20:01 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -139,6 +139,9 @@ switch (window.orientation) {
     
      document.getElementById("container").setAttribute("style", "position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 0px; left: 0; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");
      zoomOutMobile();         
+    
+     document.getElementById("MiniPlayer").style.visibility = "hidden";
+     document.getElementById("MiniPlayer").style.display = "none";
      break;  
    case 90:  
      // Landscape  (Counterclockwise)     
@@ -147,6 +150,9 @@ switch (window.orientation) {
     
      document.getElementById("container").setAttribute("style", "position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 0px; left: 0; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");
      zoomOutMobile();              
+    
+     document.getElementById("MiniPlayer").style.visibility = "hidden";
+     document.getElementById("MiniPlayer").style.display = "none";
      break;
      }
  }
