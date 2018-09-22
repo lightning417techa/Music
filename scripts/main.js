11a23955aaba52428bee09aeeddb6937746544f2
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-22 09:57:33 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -115,14 +115,14 @@ switch (window.orientation) {
    case 0:  
      // Portrait 
        //alert("Portrait 0°");
-     document.getElementById("container").setAttribute("style", "position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 20px auto; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");                                                                                           
      document.getElementById("PlaylistC").style.visibility = "hidden";
+     document.getElementById("container").setAttribute("style", "position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 20px auto; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");                                                                                               
      break;      
    case 180:  
      // Portrait (Upside-down)
        //alert("Portrait (Upside Down) 180°");
-     document.getElementById("container").setAttribute("style", "position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 20px auto; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");
      document.getElementById("PlaylistC").style.visibility = "hidden";
+     document.getElementById("container").setAttribute("style", "position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 20px auto; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");     
      break; 
    case -90:  
      // Landscape (Clockwise)
