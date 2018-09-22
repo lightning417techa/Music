fe3be183bf9800eb0b08a5a568cfc6ca8fe00695
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-22 13:21:34 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -28,18 +28,19 @@ function startUI() {
     SBTNI++;
         let NewSongBtn = document.createElement("a");
         NewSongBtn.id = SBTNI;
+        NewSongBtn.setAttribute("style", "color: white; padding: 10px 50px; text-decoration: none; text-align: left; display: block; border-top: 0.9px solid #9B9898;");     
         NewSongBtn.innerHTML = "id is " + SBTNI + "<br>";
         NewSongBtn.onclick = function() {at(NewSongBtn.id)};
         document.body.appendChild(NewSongBtn);
     }
 }
 function at(clicked_id) {
-  e = clicked_id;
+  b = clicked_id;
   alert(x);  
 }
-startUI();
 
 function Update() {
+  startUI();
   let UpdateContainer = document.createElement("div");
   UpdateContainer.id = "UC";
   UpdateContainer.classList.toggle("NewUpdate");
