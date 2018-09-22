88a7cfc8b873770edae694a2b76a36eabdd832ac
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-22 10:56:37 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -125,7 +125,7 @@ switch (window.orientation) {
    case 180:  
      // Portrait (Upside-down)
        //alert("Portrait (Upside Down) 180°");
-     document.getElementById("PlaylistC").style.visibility = "hidden;";
+     document.getElementById("PlaylistC").style.visibility = "hidden";
      document.getElementById("PlaylistC").style.display = "none";
      document.getElementById("container").setAttribute("style", "top: 5%; position: relative; width: 330px; min-height:480px; background: #333; overflow: auto; margin: 20px auto; border-radius: 10px; box-shadow: 0 10px 8px -8px #333; align-items: center; text-align: center;");     
    
