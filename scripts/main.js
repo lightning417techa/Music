b24c58c5696c8e74084f4a0027853b896590345d
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-28 17:33:28 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -78,7 +78,7 @@ function BeginUpdate() {
     var postData = {};
     let id = (0|Math.random()*9e6).toString(36)+"-"+(0|Math.random()*9e6).toString(36)+"-"+(0|Math.random()*9e6).toString(36);    
     var updates = {};
-    firebase.database().ref('LanyxSoft-Music-Update/' + id).set({
+    firebase.database().ref('LanyxSoft-Music-Update/' + localStorage.getItem("LUDIN")).set({
       updated : "true"
     });
   
