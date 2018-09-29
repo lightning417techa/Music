fcd34611c801252db148ce18d88ab690c67ee693
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-29 11:09:38 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -107,10 +107,10 @@ function SetID() {
 // Check browser support
 if (typeof(Storage) !== "undefined") {
   if (localStorage.getItem("LUDIN") == null) { 
-    console.log("func:", SetID(), "result: func success= ", true);
+    console.log("func: SetID() result:", true);
     SetID();    
   } else {
-    console.log("func: InitializeStartzup() result: func success= "+ true);
+    console.log("func: InitializeStartzup() result:", true); //console.log("func: InitializeStartzup() result: func success= "+ true);
     InitializeStartzup();              
   }
 } else {  
