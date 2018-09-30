923d90643a7e5933c8517fda1d0334ccc166440e
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-30 14:25:08 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -45,7 +45,7 @@ function createAlbums() {
         NewAlbumBtn.id = AB;
         NewAlbumBtn.setAttribute("style", "float: left; width: 25%; padding: 0 8px; text-align:center;align-items:center; display: inline-block; float: none;  white-space: nowrap; overflow: hidden; text-overflow:ellipsis;");     
         //NewAlbumBtn.innerHTML = AlbumName[AB];
-        //NewAlbumBtn.onclick = function(clicked_id) {alert(clicked_id);};
+        NewAlbumBtn.onclick = function() {PlayAlbum(clicked_id);};
         let IMG = document.createElement("img");
         IMG.src = AlbumIMG[AB];
         IMG.style.width = "100%";
@@ -55,6 +55,14 @@ function createAlbums() {
         NewAlbumBtn.appendChild(text);
         document.getElementById("albums").appendChild(NewAlbumBtn);
 }}}}
+
+function PlayAlbum(clicked_id) {
+  var id = clicked_id;
+  var AN;
+   for (AN =0; AN < 37) {}
+     AN++;
+  
+}
 
 function Update() {  
   let UpdateContainer = document.createElement("div");
