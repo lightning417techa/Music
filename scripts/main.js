971c545ab319b3a25061b7af1b520c2123149152
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-09-30 13:35:48 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -67,7 +67,7 @@ function Update() {
   var wm = "Welcome to LanyxSoft Music!";
   var wmr= wm.bold();
   Updatetxt.innerHTML = wmr + " please note that this web player is still in beta testing mode meaning that there will most likely be issues. Thank you for you coaperation.";
-  Updatetxt.setAttribute("style", "z-index: 7; position: fixed; left: 50%; top: 20%; transform: translate(-50%, -50%); text-align: center; color: black; font-size: 20px; width: 430px;")
+  Updatetxt.setAttribute("style", "z-index: 7; position: fixed; left: 50%; top: 15%; transform: translate(-50%, -50%); text-align: center; color: black; font-size: 20px; width: 430px;")
   UpdateContainer.appendChild(Updatetxt);
   //document.body.appendChild(Updatetxt);
   
@@ -77,7 +77,7 @@ function Update() {
   document.body.appendChild(Updatebackground);
   
   let UpdateScrollController = document.createElement("div");
-  UpdateScrollController.setAttribute("style", "z-index: 7; border-color: black; position: fixed; left: 50%; top: 30%; transform: translate(-50%, -50%); text-align: left; color: black; background: gray; font-size: 20px; width: 430px; height: 90px;")
+  UpdateScrollController.setAttribute("style", "z-index: 7; border-color: black; position: fixed; left: 50%; top: 35%; transform: translate(-50%, -50%); text-align: left; color: black; background: gray; font-size: 20px; width: 430px; height: 90px;")
   UpdateScrollController.id = "USC";
   UpdateContainer.appendChild(UpdateScrollController);  
   //document.body.appendChild(UpdateScrollController);
