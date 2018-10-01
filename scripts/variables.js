0d2f52ecf58c0711ad06dbaf4482f16515b26a12
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-01 15:28:16 -0500

    Update variables.js

--- scripts/variables.js
+++ scripts/variables.js
@@ -39,53 +39,15 @@ var AlbumName = {
 }
 
 var AlbumIMG = {
-  1 : "",
-  2 : "",
-  3 : "https://vignette.wikia.nocookie.net/monstercat/images/0/0c/Marshmello_-_Alone.jpg/revision/latest?cb=20160513204533",
-  4 : "",
-  5 : "https://images-na.ssl-images-amazon.com/images/I/41K3SuHNQpL._SS500.jpg",
-  6 : "",
-  7 : "",
-  8 : "",
-  9 : "https://upload.wikimedia.org/wikipedia/en/b/b6/American_Beauty_American_Psycho_cover.png",
-  10 : "",
-  11 : "https://images-na.ssl-images-amazon.com/images/I/41%2BCuqqyyvL.jpg",
-  12 : "",
-  13 : "https://images-na.ssl-images-amazon.com/images/I/41%2BCuqqyyvL.jpg",
-  14 : "https://upload.wikimedia.org/wikipedia/en/b/b6/American_Beauty_American_Psycho_cover.png",
-  15 : "",
-  16 : "",
-  17 : "",
-  18 : "",
-  19 : "",
-  20 : "",
-  21 : "",
-  22 : "",
-  23 : "https://images-na.ssl-images-amazon.com/images/I/41%2BCuqqyyvL.jpg",
-  24 : "",
-  25 : "",
-  26 : "",
-  27 : "",
-  28 : "",
-  29 : "",
-  30 : "",
-  31 : "",
-  32 : "",
-  33 : "https://images-na.ssl-images-amazon.com/images/I/41K3SuHNQpL._SS500.jpg",
-  34 : "https://iplock.weebly.com/uploads/9/5/7/3/95731436/p184.png",
-  35 : "",
-  36 : "",
-  37 : ""
+  1 : "https://images-na.ssl-images-amazon.com/images/I/41K3SuHNQpL._SS500.jpg",  
+  2 : "https://upload.wikimedia.org/wikipedia/en/b/b6/American_Beauty_American_Psycho_cover.png",
+  3 : "https://images-na.ssl-images-amazon.com/images/I/41%2BCuqqyyvL.jpg",        
+  4 : "https://iplock.weebly.com/uploads/9/5/7/3/95731436/p184.png"
 }
 
 var albumlist = {
   1 : "Mania",
   2 : "American Beauty/American Psycho",
-  3 : "",
-  4 : "",
-  5 : "",
-  6 : "",
-  7 : "",
-  8 : "",
-  9 : ""
+  3 : "Vessel",
+  4 : "Watching the Sky"  
 }
