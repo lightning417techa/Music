67974bc2bf913140a91a16636cfb6b8131cdd3e0
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-01 14:57:48 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -60,7 +60,7 @@ function PlayAlbum(clicked_id) {
   Id = clicked_id;
   var AN;  
   console.log(Id);
-  alert(Songs);
+  alert(ManiaAlbum[]);
   for (AN = 0; AN < NumberOfSongs;) {
     AN++;
     let AB = document.getElementById(AN);
