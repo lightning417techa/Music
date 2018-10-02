b9627a0f913c5ef430bf4db7b79b04283553f665
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-01 19:52:50 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,7 @@ window.onerror = function(error) {console.log(error);};
 let MiniPlayerSwitch = 0;
 function MinimizeMiniPlayer() {
  let MiniPlayer = document.getElementById("MiniPlayer");
-  if (MiniPlayerSwitch == 1) {MiniPlayerSwitch = 0; MiniPlayer.classList.remove("MiniPlayerContainer");} else {MiniPlayerSwitch = 1; MiniPlayer.classList.add("MiniPlayerContainer-minimize");}
+  if (MiniPlayerSwitch == 1) {MiniPlayerSwitch = 0; MiniPlayer.classList.remove("MiniPlayerContainer"); MiniPlayer.classList.add("MiniPlayerContainer-minimize");} else {MiniPlayerSwitch = 1; MiniPlayer.classList.remove("MiniPlayerContainer-minimize"); MiniPlayer.classList.add("MiniPlayerContainer");}
 }
 
 function startUI() {    
