3ebc40017f4162a8628a2102d515773133e1bd90
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-02 18:44:55 -0500

    Update song-list.js

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -202,24 +202,8 @@ var ArtistsImg = {
   17 : "",
   18 : "",
   19 : "",
-  20 : "",
-  21 : "",
-  22 : "",
-  23 : "",
-  24 : "",
-  25 : "",
-  26 : "",
-  27 : "",
-  28 : "",
-  29 : "",
-  30 : "",
-  31 : "",
-  32 : "",
-  33 : "",
-  34 : "",
-  35 : "",
-  36 : "https://iplock.weebly.com/uploads/9/5/7/3/95731436/p296.png",
-  37 : "https://iplock.weebly.com/uploads/9/5/7/3/95731436/p294.png"
+  20 : "https://iplock.weebly.com/uploads/9/5/7/3/95731436/p296.png",
+  21 : "https://iplock.weebly.com/uploads/9/5/7/3/95731436/p294.png"  
 }
 
 var AlbumIMG = {
