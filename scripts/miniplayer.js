9889567a7765b2fef8708cacd106f21d590989f7
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-03 15:11:02 -0500

    Update miniplayer.js

--- scripts/miniplayer.js
+++ scripts/miniplayer.js
@@ -17,11 +17,10 @@ function OpenSettings() {
   
   let CloseBtn = document.createElement("div");
   let CloseBtnI = document.createElement("i");
-  CloseBtnI.setAttribute("style", "font-size: 26px;");
   CloseBtnI.classList.toggle("fa%20fa-times");
   CloseBtn.appendChild(CloseBtnI);
   CloseBtn.id = "CloseButton";    
-  CloseBtn.setAttribute("style", "z-index: 7; position: fixed; left: 95%; top: 5%; transform: translate(-50%, -50%); text-align: right; color: black; font-size: 20px; width: 30px; height: 30px;")  
+  CloseBtn.setAttribute("style", "z-index: 7; position: relattive; left: 95%; top: 5%; transform: translate(-50%, -50%); text-align: right; color: black; font-size: 20px; width: 30px; height: 30px;")  
 }
 
 function SendFeedback() {
