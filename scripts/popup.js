d8fc5378258414603c197f53fd2fd50eace687ac
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-03 08:28:32 -0500

    Update popup.js

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,18 +1,20 @@
 function OpenSettings() {
   let MoreContainer = document.createElement("div");
-  MoreContainer.id = "hi";
+  MoreContainer.id = "MoreContainer";
   MoreContainer.classList.toggle("NewUpdate");
   document.body.appendChild(MoreContainer);
   
-  let Updatebackground = document.createElement("div");
-  Updatebackground.id = "hii";
-  Updatebackground.classList.toggle("UpdateBackground");
-  document.body.appendChild(Updatebackground);
+  let DarkBackground = document.createElement("div");
+  DarkBackground.id = "";
+  DarkBackground.classList.toggle("DarkBackground");
+  document.body.appendChild(DarkBackground);
   
   let CloseBtn = document.createElement("div");
+  let CloseBtnI = document.createElement("i");
+  CloseBtnI.classList.toggle("fa%20fa-times");
+  CloseBtn.appendChild(CloseBtnI);
   CloseBtn.id = "CloseButton";    
-  CloseBtn..setAttribute("style", "z-index: 7; position: relative; left: 95%; top: 5%; transform: translate(-50%, -50%); text-align: right; color: black; font-size: 20px; width: 30px; height: 30px;")
-  
+  CloseBtn..setAttribute("style", "z-index: 7; position: relative; left: 95%; top: 5%; transform: translate(-50%, -50%); text-align: right; color: black; font-size: 20px; width: 30px; height: 30px;")  
 }
 
 
