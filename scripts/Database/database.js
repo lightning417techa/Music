dc32b0c46b4c72f081de60b4bce7471f8c77e4a9
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-08 15:14:31 -0500

    Update database.js

--- scripts/Database/database.js
+++ scripts/Database/database.js
@@ -61,15 +61,4 @@ var DatabaseMusic = [{
    }, {
     "title": "song title", "source": "test.mp3"	
   }]
-}, {
-  "title": "title",
-  "artist": "artist",
-  "artwork": "artwork",
-  "tracks": [{
-    "title": "song title", "source": "test.mp3"	
-   }, {
-    "title": "song title", "source": "test.mp3"	
-   }, {
-    "title": "song title", "source": "test.mp3"	
-  }]
 }];
