51a5253af53dae5ddc199d777debe8407804e6f1
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-08 12:49:44 -0500

    Create database.js

---
+++ scripts/database.js
@@ -0,0 +1,75 @@
+//ARTISTS (NO REPEAT)
+var DatabaseArtists = {
+  1 : "Fall Out Boy",
+  2 : "Post Malone",
+  3 : "Logic",
+  4 : "Marshmello",
+  5 : "BTS",
+  6 : "Dodie Clark",
+  7 : "Migos",
+  8 : "Sheppard",
+  9 : "Walk the Moon",
+  10 : "Cardi B",
+  11 : "Owl City",
+  12 : "Bastille",
+  13 : "Twentyone Pilots",
+  14 : "Lil Dicky",
+  15 : "Imagine Dragons",
+  16 : "Megan Trainer",
+  17 : "LAUV",
+  18 : "Ridley Nelson",
+  19 : "blank",
+  20 : "Lightning417techa",
+  21 : "Banana Tazer",  
+  22 : "Lil Pump",
+  23 : "Vomiting Pug",
+  24 : "Alan Walker",
+  25 : "MKTO"
+}
+
+//ALBUMS
+var DatabaseMusic = [{
+  "title": "Mania",
+  "artist": Artists[1],
+  "artwork": "artwork",
+  "tracks": [{
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+  }]
+}, {
+  "title": "American Beatuty/American Psycho",
+  "artist": Artists[1],
+  "artwork": "artwork",
+  "tracks": [{
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+  }]
+}, {
+  "title": "title",
+  "artist": "artist",
+  "artwork": "artwork",
+  "tracks": [{
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+  }]
+}, {
+  "title": "title",
+  "artist": "artist",
+  "artwork": "artwork",
+  "tracks": [{
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+   }, {
+    "title": "song title", "source": "test.mp3"	
+  }]
+}];
