eb432f456fa123d04f8544fc654b8e5c0e27cb4d
lightning417techa <35784379+lightning417techa@users.noreply.github.com>
2018-10-08 20:00:17 -0500

    Update miniplayer.js

--- scripts/miniplayer.js
+++ scripts/miniplayer.js
@@ -1,8 +1,17 @@
 let MiniPlayerSwitch = 0;
 function MinimizeMiniPlayer() {
  let MiniPlayer = document.getElementById("MiniPlayer");
-  if (MiniPlayerSwitch == 1) {MiniPlayerSwitch = 0; document.getElementById("MiniPlayerArrow").classList.remove("fa%20fa-angle-down"); document.getElementById("MiniPlayerArrow").classList.add("fa%20fa-angle-up"); MiniPlayer.classList.remove("MiniPlayerContainer-maximize"); MiniPlayer.classList.remove("MiniPlayerContainer"); MiniPlayer.classList.add("MiniPlayerContainer-minimize");} else {MiniPlayerSwitch = 1; document.getElementById("MiniPlayerArrow").classList.remove("fa%20fa-angle-up"); MiniPlayer.classList.remove("MiniPlayerContainer-minimize"); MiniPlayer.classList.add("MiniPlayerContainer-maximize"); document.getElementById("MiniPlayerArrow").classList.add("fa%20fa-angle-down");}
+  if (MiniPlayerSwitch == 1) {MiniPlayerSwitch = 0; document.getElementById("MiniPlayerArrow").classList.remove("fa%20fa-angle-down"); document.getElementById("MiniPlayerArrow").classList.add("fa%20fa-angle-up"); MiniPlayer.classList.remove("MiniPlayerContainer-maximize"); MiniPlayer.classList.remove("MiniPlayerContainer"); MiniPlayer.classList.add("MiniPlayerContainer-minimize"); MiniPlayerSmall();} else {MiniPlayerBig(); MiniPlayerSwitch = 1; document.getElementById("MiniPlayerArrow").classList.remove("fa%20fa-angle-up"); MiniPlayer.classList.remove("MiniPlayerContainer-minimize"); MiniPlayer.classList.add("MiniPlayerContainer-maximize"); document.getElementById("MiniPlayerArrow").classList.add("fa%20fa-angle-down");}
 }
+
+function MiniPlayerSmall() {
+  document.getElementById().style.display = "none";
+}
+
+function MiniPlayerBig() {
+  document.getElementById().style.display = "";
+}
+
 
 function OpenSettings() {
   let betatxt = " Beta"; let beta = betatxt.bold();
